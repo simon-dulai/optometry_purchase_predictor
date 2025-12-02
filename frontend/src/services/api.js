38973b1cd2b4be5eb0be5b64f89ccce1bbcb0672
Simon Dulai <ssd97@bath.ac.uk>
2025-12-02 13:17:27 +0000

    database connection 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL?.replace(/\/$/, '') || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },

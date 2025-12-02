8e7efd30ad30b8436e5fabd5971eb3c761837955
Simon Dulai <ssd97@bath.ac.uk>
2025-12-02 12:45:52 +0000

    Link frontend to backend url for predictor 2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -102,7 +102,7 @@ const Login = ({ onLogin }) => {
         </div>
 
         <div className="text-center mt-1">
-            <a href={`${import.meta.env.VITE_API_URL}/predictor2.html`} style={{ color: '#ff00ff', textDecoration: 'none', fontSize: '0.9rem' }}>
+            <a href={`${import.meta.env.VITE_API_URL} predictor2.html`} style={{ color: '#ff00ff', textDecoration: 'none', fontSize: '0.9rem' }}>
              Use Legacy Predictor
             </a>
         </div>
